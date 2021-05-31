25e7fdcac6eb5e73925980f173ca1a9ed585239b
amirshokh <amirshoxmuxitdinov@gmail.com>
2021-05-31 14:26:22 +0500

    Initial commit

---
+++ todo/models.py
@@ -0,0 +1,25 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Todo(models.Model):
+    NEW = 'new'
+    FINISHED = 'finished'
+    STATUS = [
+        (NEW, 'new'),
+        (FINISHED, 'finished')
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todos')
+    title = models.CharField(max_length=120)
+    description = models.CharField(max_length=512)
+    status = models.CharField(choices=STATUS, max_length=10, default=NEW)
+
+    def mark_as_finished(self):
+        self.status = self.FINISHED
+        self.save()
+
+    def mark_as_unfinished(self):
+        self.status = self.NEW
+        self.save()
